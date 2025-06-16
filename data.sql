60f876a262ed632a45f0129508a580fe6f510868
Joaquin <jnruiz@fi.uba.a>
2025-06-16 18:08:58 -0300

    AGREGUE AL SCRIPT VALORES PARA QUE CUANDO LEVANTEMOS LA BASE DE DATOS LAS TABLAS TENGAN VALORES

--- data.sql
+++ data.sql
@@ -1,26 +1,3 @@
-create table empresa (
-id serial primary key,
-nombre VARCHAR(50) not null
-,email VARCHAR(50) not null
-,direccion VARCHAR(50) not null
-,intereses VARCHAR(255),
-reputacion VARCHAR(50)
-);
-
-
-create table objeto (
-id serial primary key,
-titulo VARCHAR(50) not null,
-descripcion VARCHAR(255) not null,
-categoria VARCHAR(50) not null,
-estado VARCHAR(50),
-empresa_id INT not null,
-CONSTRAINT fk_empresa
-	FOREIGN KEY (empresa_id)
-	REFERENCES empresa(id)
-	ON DELETE CASCADE
-);
-
 -- Insertar empresas
 INSERT INTO empresa (nombre, email, direccion, intereses, reputacion) VALUES
 ('TechNova S.A.', 'contacto@technova.com', 'Av. Corrientes 1234', 'Electrónica, Computación', 'Buena'),
