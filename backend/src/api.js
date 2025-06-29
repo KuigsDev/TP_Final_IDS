dea8d0b53882e0a2a9a6446c86680b5c38ef5bd8
KuigsDev <axel.julian.gonzalez360@gmail.com>
2025-06-29 18:23:28 -0300

    test

--- backend/src/api.js
+++ backend/src/api.js
@@ -7,6 +7,19 @@ app.get("/api/health",(req,res)=>{
     res.json({status:"OK"})
 });
 
+//get all
+app.get("/api/usuarios",(req,res)=>{
+    res.json({status:"OK"})
+});
+//get one
+app.get("/api/usuarios/:id",(req,res)=>{
+    res.json({status:"OK"})
+});
+//insert
+app.get("/api/usuarios",(req,res)=>{
+    res.json({status:"OK"})
+});
+
 app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
 });
